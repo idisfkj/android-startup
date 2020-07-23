d8eb80baa9a0dc4e58c9db393b1f2daa4d2cc093
idisfkj <idisfkj@gmail.com>
2020-07-23 11:34:26 +0800

    feat: init build dependencies

---
+++ buildSrc/src/main/java/Dependencies.kt
@@ -0,0 +1,40 @@
+import org.gradle.api.artifacts.dsl.RepositoryHandler
+
+/**
+ * Created by idisfkj on 2020/7/23.
+ * Email: idisfkj@gmail.com.
+ */
+object Versions {
+
+    const val compile_sdk_version = 29
+    const val min_sdk_version = 15
+    const val target_sdk_version = 29
+    const val version_code = 1
+    const val version_name = "1.0"
+
+    const val gradle = "4.0.1"
+    const val kotlin = "1.3.72"
+    const val core_ktx = "1.3.1"
+    const val appcompat = "1.1.0"
+    const val junit = "4.12"
+    const val ext_junit = "1.1.1"
+    const val espresso_core = "3.2.0"
+    const val constraint_layout = "1.1.3"
+}
+
+object Dependencies {
+    const val build_gradle = "com.android.tools.build:gradle:${Versions.gradle}"
+    const val kotlin_gradle_plugin = "org.jetbrains.kotlin:kotlin-gradle-plugin:${Versions.kotlin}"
+    const val kotlin_stdlib = "org.jetbrains.kotlin:kotlin-stdlib:${Versions.kotlin}"
+    const val core_ktx = "androidx.core:core-ktx:${Versions.core_ktx}"
+    const val appcompat = "androidx.appcompat:appcompat:${Versions.appcompat}"
+    const val junit = "junit:junit:${Versions.junit}"
+    const val ext_junit = "androidx.test.ext:junit:${Versions.ext_junit}"
+    const val espresso_core = "androidx.test.espresso:espresso-core:${Versions.espresso_core}"
+    const val constraint_layout = "androidx.constraintlayout:constraintlayout:${Versions.constraint_layout}"
+
+    val addRepos: (handler: RepositoryHandler) -> Unit = {
+        it.google()
+        it.jcenter()
+    }
+}
