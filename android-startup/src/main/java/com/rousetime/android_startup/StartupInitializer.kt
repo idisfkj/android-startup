2403d842da862f279e01a0a09ae0b6a9b093cc12
idisfkj <idisfkj@gmail.com>
2020-08-07 17:23:40 +0800

    feat: Extract discover and Initialize logic from StartupProvider into StartupInitializer.

---
+++ android-startup/src/main/java/com/rousetime/android_startup/StartupInitializer.kt
@@ -0,0 +1,76 @@
+package com.rousetime.android_startup
+
+import android.content.ComponentName
+import android.content.Context
+import android.content.pm.PackageManager
+import com.rousetime.android_startup.execption.StartupException
+import com.rousetime.android_startup.model.StartupProviderStore
+import com.rousetime.android_startup.provider.StartupProvider
+import com.rousetime.android_startup.provider.StartupProviderConfig
+
+/**
+ * Created by idisfkj on 2020/8/7.
+ * Email: idisfkj@gmail.com.
+ */
+class StartupInitializer {
+
+    companion object {
+        @JvmStatic
+        val instance by lazy { StartupInitializer() }
+    }
+
+    internal fun discoverAndInitialize(context: Context): StartupProviderStore {
+        val result = mutableListOf<AndroidStartup<*>>()
+        val initialize = mutableListOf<Class<out Startup<*>>>()
+        val initialized = mutableListOf<Class<out Startup<*>>>()
+        var config: StartupProviderConfig? = null
+        try {
+            val provider = ComponentName(context.packageName, StartupProvider::class.java.name)
+            val providerInfo = context.packageManager.getProviderInfo(provider, PackageManager.GET_META_DATA)
+            val startup = context.getString(R.string.android_startup)
+            val providerConfig = context.getString(R.string.android_startup_provider_config)
+            providerInfo.metaData?.let { metaData ->
+                metaData.keySet().forEach { key ->
+                    val value = metaData[key]
+                    val clazz = Class.forName(key)
+                    if (startup == value) {
+                        if (AndroidStartup::class.java.isAssignableFrom(clazz)) {
+                            doInitialize((clazz.getDeclaredConstructor().newInstance() as AndroidStartup<*>), result, initialize, initialized)
+                        }
+                    } else if (providerConfig == value) {
+                        if (StartupProviderConfig::class.java.isAssignableFrom(clazz)) {
+                            config = clazz.getDeclaredConstructor().newInstance() as StartupProviderConfig?
+                        }
+                    }
+                }
+            }
+        } catch (t: Throwable) {
+            throw StartupException(t)
+        }
+        return StartupProviderStore(result, config)
+    }
+
+    private fun doInitialize(
+        startup: AndroidStartup<*>,
+        result: MutableList<AndroidStartup<*>>,
+        initialize: MutableList<Class<out Startup<*>>>,
+        initialized: MutableList<Class<out Startup<*>>>
+    ) {
+        try {
+            if (initialize.contains(startup::class.java)) {
+                throw IllegalStateException("have circle dependencies.")
+            }
+            if (!initialized.contains(startup::class.java)) {
+                initialize.add(startup::class.java)
+                result.add(startup)
+                startup.dependencies()?.forEach {
+                    doInitialize(it.getDeclaredConstructor().newInstance() as AndroidStartup<*>, result, initialize, initialized)
+                }
+                initialize.remove(startup::class.java)
+                initialized.add(startup::class.java)
+            }
+        } catch (t: Throwable) {
+            throw StartupException(t)
+        }
+    }
+}
--- android-startup/src/main/java/com/rousetime/android_startup/provider/StartupProvider.kt
+++ android-startup/src/main/java/com/rousetime/android_startup/provider/StartupProvider.kt
@@ -1,19 +1,13 @@
 package com.rousetime.android_startup.provider
 
-import android.content.ComponentName
 import android.content.ContentProvider
 import android.content.ContentValues
-import android.content.pm.PackageManager
 import android.database.Cursor
 import android.net.Uri
-import com.rousetime.android_startup.AndroidStartup
-import com.rousetime.android_startup.R
-import com.rousetime.android_startup.Startup
+import com.rousetime.android_startup.StartupInitializer
 import com.rousetime.android_startup.StartupManager
 import com.rousetime.android_startup.execption.StartupException
-import com.rousetime.android_startup.model.LoggerLevel
 import com.rousetime.android_startup.model.StartupConfig
-import com.rousetime.android_startup.model.StartupProviderStore
 
 /**
  * Created by idisfkj on 2020/7/28.
@@ -23,11 +17,9 @@ class StartupProvider : ContentProvider() {
 
     override fun onCreate(): Boolean {
         context.takeIf { context -> context != null }?.let {
-            val store = discoverAndInitialize()
+            val store = StartupInitializer.instance.discoverAndInitialize(it)
             StartupManager.Builder()
-                .setConfig(store.config?.getConfig()?: StartupConfig.Builder().build())
-//                .setAwaitTimeout(store.config?.getAwaitTimeout() ?: StartupManager.AWAIT_TIMEOUT)
-//                .setLoggerLevel(store.config?.getLoggerLevel() ?: LoggerLevel.NONE)
+                .setConfig(store.config?.getConfig() ?: StartupConfig.Builder().build())
                 .addAllStartup(store.result)
                 .build(it)
                 .start()
@@ -55,62 +47,5 @@ class StartupProvider : ContentProvider() {
 
     override fun getType(uri: Uri): String? {
         throw IllegalStateException("Not allowed.")
-    }
-
-    private fun discoverAndInitialize(): StartupProviderStore {
-        val result = mutableListOf<AndroidStartup<*>>()
-        val initialize = mutableListOf<Class<out Startup<*>>>()
-        val initialized = mutableListOf<Class<out Startup<*>>>()
-        var config: StartupProviderConfig? = null
-        try {
-            context?.let {
-                val provider = ComponentName(it.packageName, this::class.java.name)
-                val providerInfo = it.packageManager.getProviderInfo(provider, PackageManager.GET_META_DATA)
-                val startup = it.getString(R.string.android_startup)
-                val providerConfig = it.getString(R.string.android_startup_provider_config)
-                providerInfo.metaData?.let { metaData ->
-                    metaData.keySet().forEach { key ->
-                        val value = metaData[key]
-                        val clazz = Class.forName(key)
-                        if (startup == value) {
-                            if (AndroidStartup::class.java.isAssignableFrom(clazz)) {
-                                doInitialize((clazz.getDeclaredConstructor().newInstance() as AndroidStartup<*>), result, initialize, initialized)
-                            }
-                        } else if (providerConfig == value) {
-                            if (StartupProviderConfig::class.java.isAssignableFrom(clazz)) {
-                                config = clazz.getDeclaredConstructor().newInstance() as StartupProviderConfig?
-                            }
-                        }
-                    }
-                }
-            }
-        } catch (t: Throwable) {
-            throw StartupException(t)
-        }
-        return StartupProviderStore(result, config)
-    }
-
-    private fun doInitialize(
-        startup: AndroidStartup<*>,
-        result: MutableList<AndroidStartup<*>>,
-        initialize: MutableList<Class<out Startup<*>>>,
-        initialized: MutableList<Class<out Startup<*>>>
-    ) {
-        try {
-            if (initialize.contains(startup::class.java)) {
-                throw IllegalStateException("have circle dependencies.")
-            }
-            if (!initialized.contains(startup::class.java)) {
-                initialize.add(startup::class.java)
-                result.add(startup)
-                startup.dependencies()?.forEach {
-                    doInitialize(it.getDeclaredConstructor().newInstance() as AndroidStartup<*>, result, initialize, initialized)
-                }
-                initialize.remove(startup::class.java)
-                initialized.add(startup::class.java)
-            }
-        } catch (t: Throwable) {
-            throw StartupException(t)
-        }
     }
 }
